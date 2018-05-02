b41b29993f535d041326af5214539b57993911d8
PowWow Admin <admin@studio.dev.powwowinc.net>
2018-05-02 16:32:10 +0300

    [Connect] Create Exploration

--- server/client.js
+++ server/client.js
@@ -1,3 +1,4 @@
 exports.start = async (session, models, vars) => {
     await session.transform.changepassword.start();
+    await session.transform.cb.start();
 };
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const session = require('smartux-connect');
 session.transform.changepassword = session.loadTransform('changepassword');
+session.transform.cb = session.loadTransform('cb');
 require('./client');
 session.start();
