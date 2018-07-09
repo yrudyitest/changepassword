57e94bc413426899c061c73edb44081122b163e5
PowWow Admin <admin@studio.dev.powwowinc.net>
2018-07-09 13:00:38 +0300

    [Studio] Create Exploration

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const session = require('smartux-connect');
+session.transform.changepassword = session.loadTransform('changepassword');
 require('./client');
 require('./transform/changepassword');
 session.start();
