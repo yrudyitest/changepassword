8bb3564440ec7ffe5b3f656d3119a3e4fff912ea
PowWow Admin <admin@studio.dev.powwowinc.net>
2018-07-09 12:53:01 +0300

    [Explorer] Delete exploration

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const session = require('smartux-connect');
-session.transform.changepassword = session.loadTransform('changepassword');
 require('./client');
 require('./transform/changepassword');
 session.start();
