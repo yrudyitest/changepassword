27ebbfd8c6250b748ea7275b0ca319dc4e7675ea
PowWow Admin <admin@studio.dev.powwowinc.net>
2018-07-10 14:05:40 +0300

    [Connect] Saved endpoint "client.sdfsdg"

---
+++ server/client/sdfsdg.js
@@ -0,0 +1,2 @@
+exports.onload = async (session, models, vars) => {
+};
--- server/index.js
+++ server/index.js
@@ -2,4 +2,5 @@ const session = require('smartux-connect');
 session.transform.changepassword = session.loadTransform('changepassword');
 require('./client');
 require('./transform/changepassword');
+require('./client/sdfsdg');
 session.start();
