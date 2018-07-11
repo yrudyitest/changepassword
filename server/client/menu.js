26cb2652c2e0524be34871f1d7fdf9e43e25690c
PowWow Admin <admin@studio.dev.powwowinc.net>
2018-07-11 13:30:28 +0300

    [Connect] Saved endpoint "client.menu"

--- server/client/menu.js
+++ server/client/menu.js
@@ -1,6 +1,7 @@
 const trigger = require('smartux-connect').trigger;
 trigger({
-    id: 'transform.changepassword.undefined.dialog_confirm',
+    id: 'transform.changepassword.undefined.dialog_confirm1',
     events: ['transform.changepassword.undefined.dialog_confirm']
 }, async (session, models, vars) => {
+    await session.alert();
 });
